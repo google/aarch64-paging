950184850708636df0b41d153efdf94c560b721f
Andrew Walbran <qwandor@google.com>
2024-09-24 10:52:32 +0100

    Move attributes to submodule.

--- src/paging.rs
+++ src/paging.rs
@@ -5,9 +5,12 @@
 //! Generic aarch64 page table manipulation functionality which doesn't assume anything about how
 //! addresses are mapped.
 
+mod attributes;
+
 use crate::MapError;
 #[cfg(feature = "alloc")]
 use alloc::alloc::{alloc_zeroed, dealloc, handle_alloc_error, Layout};
+pub use attributes::Attributes;
 use bitflags::bitflags;
 use core::fmt::{self, Debug, Display, Formatter};
 use core::marker::PhantomData;
@@ -510,53 +513,6 @@ impl Iterator for ChunkedIterator<'_> {
         self.start = end;
         Some(c)
     }
-}
-
-bitflags! {
-    /// Attribute bits for a mapping in a page table.
-    #[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-    pub struct Attributes: usize {
-        const VALID         = 1 << 0;
-        const TABLE_OR_PAGE = 1 << 1;
-
-        const ATTRIBUTE_INDEX_0 = 0 << 2;
-        const ATTRIBUTE_INDEX_1 = 1 << 2;
-        const ATTRIBUTE_INDEX_2 = 2 << 2;
-        const ATTRIBUTE_INDEX_3 = 3 << 2;
-        const ATTRIBUTE_INDEX_4 = 4 << 2;
-        const ATTRIBUTE_INDEX_5 = 5 << 2;
-        const ATTRIBUTE_INDEX_6 = 6 << 2;
-        const ATTRIBUTE_INDEX_7 = 7 << 2;
-
-        const OUTER_SHAREABLE = 2 << 8;
-        const INNER_SHAREABLE = 3 << 8;
-
-        const NS            = 1 << 5;
-        const USER          = 1 << 6;
-        const READ_ONLY     = 1 << 7;
-        const ACCESSED      = 1 << 10;
-        const NON_GLOBAL    = 1 << 11;
-        const DBM           = 1 << 51;
-        /// Privileged Execute-never, if two privilege levels are supported.
-        const PXN           = 1 << 53;
-        /// Unprivileged Execute-never, or just Execute-never if only one privilege level is
-        /// supported.
-        const UXN           = 1 << 54;
-
-        // Software flags in block and page descriptor entries.
-        const SWFLAG_0 = 1 << 55;
-        const SWFLAG_1 = 1 << 56;
-        const SWFLAG_2 = 1 << 57;
-        const SWFLAG_3 = 1 << 58;
-    }
-}
-
-impl Attributes {
-    /// Mask for the bits determining the shareability of the mapping.
-    pub const SHAREABILITY_MASK: Self = Self::INNER_SHAREABLE;
-
-    /// Mask for the bits determining the attribute index of the mapping.
-    pub const ATTRIBUTE_INDEX_MASK: Self = Self::ATTRIBUTE_INDEX_7;
 }
 
 /// Smart pointer which owns a [`PageTable`] and knows what level it is at. This allows it to

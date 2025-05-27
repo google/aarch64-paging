3068f07054c59805a5e0c57996db58fb1febeff7
Ard Biesheuvel <ardb@kernel.org>
2025-05-27 20:22:57 +0200

    Perform TLB invalidation after updating live page tables
    When modifying page tables that are inactive, no TLB maintenance is
    necessary, as those page tables have either never been seen by the page
    table walker, or they were deactivated, at which point TLB maintenance
    has been performed.
    
    However, when modifying live page tables, no TLB maintenance is being
    performed , which means that stale translations may linger indefinitely.
    
    So when mapping ranges in a live set of translation tables, visit the
    entire mapped range again to invalidate each chunk in the TLBs. (Note
    that the architecture provides strong guarantees that a block mapping
    may be invalidated by a single TLBI by virtual address targetting any
    virtual address in the range covered by the mapping.)
    
    When modifying ranges, perform each TLBI as the descriptors are being
    manipulated, and only when that results in its value to change.

--- src/paging.rs
+++ src/paging.rs
@@ -9,6 +9,8 @@ use crate::MapError;
 #[cfg(feature = "alloc")]
 use alloc::alloc::{Layout, alloc_zeroed, dealloc, handle_alloc_error};
 use bitflags::bitflags;
+#[cfg(all(not(test), target_arch = "aarch64"))]
+use core::arch::asm;
 use core::fmt::{self, Debug, Display, Formatter};
 use core::marker::PhantomData;
 use core::ops::{Add, Range, Sub};
@@ -59,6 +61,36 @@ impl TranslationRegime {
     /// This also implies that it supports two VA ranges.
     pub(crate) fn supports_asid(self) -> bool {
         matches!(self, Self::El2And0 | Self::El1And0)
+    }
+
+    /// Invalidates the memory range starting at `va`. The size of the range is unspecified, it is
+    /// up to the caller to apply this function as needed to invalidate a range of memory
+    pub(crate) fn invalidate_va(self, va: VirtualAddress) {
+        #[allow(unused)]
+        let va = va.0 >> 12;
+
+        // SAFETY: TLBI maintenance has no side effects that are observeable by the
+        // program
+        #[cfg(all(not(test), target_arch = "aarch64"))]
+        unsafe {
+            match self {
+                TranslationRegime::El3 => asm!(
+                    "tlbi vae3is, {va}",
+                    va = in(reg) va,
+                    options(preserves_flags, nostack),
+                ),
+                TranslationRegime::El2 | TranslationRegime::El2And0 => asm!(
+                    "tlbi vae2is, {va}",
+                    va = in(reg) va,
+                    options(preserves_flags, nostack),
+                ),
+                TranslationRegime::El1And0 => asm!(
+                    "tlbi vaae1is, {va}",
+                    va = in(reg) va,
+                    options(preserves_flags, nostack),
+                ),
+            };
+        };
     }
 }
 
@@ -377,15 +409,23 @@ impl<T: Translation> RootTable<T> {
     ///
     /// Returns [`MapError::AddressRange`] if the largest address in the `range` is greater than the
     /// largest virtual address covered by the page table given its root level.
-    ///
-    /// Returns [`MapError::BreakBeforeMakeViolation'] if the range intersects with live mappings,
-    /// and modifying those would violate architectural break-before-make (BBM) requirements.
-    pub fn modify_range<F>(&mut self, range: &MemoryRegion, f: &F) -> Result<(), MapError>
+    pub(crate) fn modify_range<F>(
+        &mut self,
+        range: &MemoryRegion,
+        f: &F,
+        live: bool,
+    ) -> Result<bool, MapError>
     where
         F: Fn(&MemoryRegion, &Descriptor, usize) -> Result<DescriptorBits, ()> + ?Sized,
     {
         self.verify_region(range)?;
-        self.table.modify_range(&mut self.translation, range, f)
+        self.table.modify_range(
+            &mut self.translation,
+            self.translation_regime,
+            range,
+            f,
+            live,
+        )
     }
 
     /// Applies the provided callback function to the page table descriptors covering a given
@@ -774,12 +814,15 @@ impl<T: Translation> PageTableWithLevel<T> {
     fn modify_range<F>(
         &mut self,
         translation: &mut T,
+        translation_regime: TranslationRegime,
         range: &MemoryRegion,
         f: &F,
-    ) -> Result<(), MapError>
+        live: bool,
+    ) -> Result<bool, MapError>
     where
         F: Fn(&MemoryRegion, &Descriptor, usize) -> Result<DescriptorBits, ()> + ?Sized,
     {
+        let mut modified = false;
         let level = self.level;
         for chunk in range.split(level) {
             let entry = self.get_entry_mut(chunk.0.start);
@@ -792,14 +835,20 @@ impl<T: Translation> PageTableWithLevel<T> {
                     None
                 }
             }) {
-                subtable.modify_range(translation, &chunk, f)?;
-            } else {
-                entry.assign(
-                    f(&chunk, entry, level).map_err(|_| MapError::PteUpdateFault(entry.bits()))?,
-                );
+                modified |=
+                    subtable.modify_range(translation, translation_regime, &chunk, f, live)?;
+            } else if entry.assign(
+                // The descriptor will not be modified if f() returns an error, so no TLB
+                // maintenance is needed in that case.
+                f(&chunk, entry, level).map_err(|_| MapError::PteUpdateFault(entry.bits()))?,
+            ) && live
+            {
+                // Live descriptor was updated so TLB maintenance is needed
+                translation_regime.invalidate_va(chunk.start());
+                modified = true;
             }
         }
-        Ok(())
+        Ok(modified)
     }
 
     /// Walks a range of page table entries and passes each one to a caller provided function.
